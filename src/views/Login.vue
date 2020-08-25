50702a2635f99e1eb05976e847e19e0a62159f92
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-25 19:13:25 +0300

    Init

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        <input type="text" placeholder="login">Login
+        <input type="password" placeholder="password">Password
+        <button>Sign in</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
