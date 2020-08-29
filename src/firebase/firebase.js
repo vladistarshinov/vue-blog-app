6dbe05439ac9ada35b3befe5ef3e5b527212343a
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-29 18:04:15 +0300

    Fix comments view

--- src/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBPtPZ5NO-JQ8POcIlhNZCNk5bqEoXq3W8',
-  authDomain: 'vue-blog-app-9739f.firebaseapp.com',
-  databaseURL: 'https://vue-blog-app-9739f.firebaseio.com',
-  projectId: 'vue-blog-app-9739f',
-  storageBucket: 'vue-blog-app-9739f.appspot.com',
-  messagingSenderId: '411676759099',
-  appId: '1:411676759099:web:f4fcc88e673ce294a8381e'
-}
